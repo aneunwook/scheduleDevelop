00a15e216104f756fffecaf97bd07475e67c8918
aneunwook <dmsdnr0720@naver.com>
2025-05-19 16:40:57 +0900

    feat : 일정 조회 api 추가

---
+++ src/main/java/org/example/scheduledevelop/dto/scheduledto/ScheduleResponseDto.java
@@ -0,0 +1,28 @@
+package org.example.scheduledevelop.dto.scheduledto;
+
+import lombok.Getter;
+import org.example.scheduledevelop.entity.Schedule;
+
+@Getter
+public class ScheduleResponseDto {
+    private final Long id;
+    private final String username;
+    private final String title;
+    private final String contents;
+
+    public ScheduleResponseDto(Long id, String username, String title, String contents) {
+        this.id = id;
+        this.username = username;
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public static ScheduleResponseDto toDto(Schedule schedule) {
+        return new ScheduleResponseDto(
+                schedule.getId(),
+                schedule.getUsername(),
+                schedule.getTitle(),
+                schedule.getContents()
+        );
+    }
+}
